ae3e427a6ee421e0a4a5cc91d106cd0cb44cb2a2
lucas-tremaroli <lucasluigitremaroli@outlook.com>
2025-05-08 01:13:19 +0200

    chore: refactor

--- app/api/endpoints/production.py
+++ app/api/endpoints/production.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends
 from app.core.auth import validate_api_key
-from app.services.production_service import get_production_data
+from app.services.production_service import ProductionService
+
+ps = ProductionService()
 
 router = APIRouter(
     prefix="/production",
@@ -12,14 +14,6 @@ router = APIRouter(
 async def ping(
         api_key: str = Depends(validate_api_key)
 ):
-    table = get_production_data(2023)
-    target_rows = table.select('tr:has(td.tb_subitem:nth-child(1))'
-                              ':has(td.tb_subitem:nth-child(2))')
-    data = {}
-    for row in target_rows:
-        cells = row.find_all('td', class_='tb_subitem')
-        if len(cells) == 2:
-            product = cells[0].text.strip()
-            quantity = cells[1].text.strip()
-            data[product] = quantity
+    table = ps.get_production_data(year=2023)
+    data = ps.parse(table)
     return data
--- app/services/production_service.py
+++ app/services/production_service.py
@@ -1,12 +1,30 @@
-# This module contains the business logic for scrapping the production data.
-
 import bs4
 import requests
 
 
-def get_production_data(year: int) -> bs4.BeautifulSoup:
-    url = f"http://vitibrasil.cnpuv.embrapa.br/index.php?ano={year}&opcao=opt_02"
-    response = requests.get(url).content
-    soup = bs4.BeautifulSoup(response, "lxml")
-    table = soup.find("table", class_="tb_dados")
-    return table
+class ProductionService:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_production_data(year: int) -> bs4.BeautifulSoup:
+        url = f"http://vitibrasil.cnpuv.embrapa.br/index.php?ano={year}&opcao=opt_02"
+        response = requests.get(url).content
+        soup = bs4.BeautifulSoup(response, "lxml")
+        table = soup.find("table", class_="tb_dados")
+        return table
+
+    @staticmethod
+    def parse(table: bs4.BeautifulSoup):
+        target_rows = table.select(
+            'tr:has(td.tb_subitem:nth-child(1))'
+            ':has(td.tb_subitem:nth-child(2))'
+        )
+        data = {}
+        for row in target_rows:
+            cells = row.find_all('td', class_='tb_subitem')
+            if len(cells) == 2:
+                product = cells[0].text.strip()
+                quantity = cells[1].text.strip()
+                data[product] = quantity
+        return data
