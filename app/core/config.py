5f09d0a1480f0a9a0589839ac78bbca8a6cf74a7
lucas-tremaroli <lucasluigitremaroli@outlook.com>
2025-05-27 01:16:55 +0200

    doc: improve swagger

--- app/core/config.py
+++ app/core/config.py
@@ -3,19 +3,28 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 markdown_description = """
-## Authentication
 
-Click on **Authorize** and use the following credentials:
+### Description
 
-- **User**: fiap
-- **Password**: secret
+This API provides authenticated access to production, processing, and commercial data related to grape and wine-growing, sourced from Embrapa's website. It's designed to serve clean, structured JSON data for machine learning models, dashboards, and notebooks.
+
+### Authentication
+
+To access protected endpoints, you first need to authenticate.
+
+1.  Click 'Authorize' and enter the following credentials:
+
+    -   **Username**: `fiap`
+    -   **Password**: `secret`
+
+3.  Once authorized, you can access all protected data endpoints by clicking on 'Try it out'.
 """
 
 class Settings(BaseSettings):
-    title: str = "Tech Challange API"
+    title: str = "Vitiviniculture Data API"
     description: str = markdown_description
     version: str = "0.1.0"
-    summary: str = "This is a FastAPI project for the phase one tech challenge."
+    summary: str = "A RESTful API for vitiviniculture data retrieval and analysis, developed as the first tech challenge for FIAP's ML course."
     contact: dict = {
         "name": "GitHub project",
         "url": "https://github.com/lucas-tremaroli/tech-challenge-one",
